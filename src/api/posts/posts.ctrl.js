d7a4b58f29271c961fcec56fcd52bc6b232e7f79
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-24 16:08:27 +0900

    updated

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -64,4 +64,27 @@ export const remove = async (ctx) => {
         ctx.throw(500, e);
     }
 };
-export const update = (ctx) => {};
+
+/*
+    PATCH /api/posts/:id
+    {
+        title: 수정,
+        body: 내용,
+        tags: [수정, 태그]
+    }
+*/
+export const update = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true,
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
