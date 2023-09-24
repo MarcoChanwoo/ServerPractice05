33725e9bcf829d81fb04b115cd5d46b4b2ca1d84
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-24 17:36:06 +0900

    updated

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -34,10 +34,51 @@ export const register = async (ctx) => {
 
         // 응답할 데이터에서 hashedPassword 필드 제거
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }
 };
-export const login = async (ctx) => {};
+
+/*
+    POST /api/auth/login
+    {
+        username: marco,
+        password: my123
+    }
+*/
+export const login = async (ctx) => {
+    const { username, password } = ctx.request.body;
+    if (!username || !password) {
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+    try {
+        const user = await User.findByUsername(username);
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true,
+        });
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 export const check = async (ctx) => {};
 export const logout = async (ctx) => {};
