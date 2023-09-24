616dc193f9abf84a32d1bf8587cce299acfd7ae0
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-24 16:00:56 +0900

    updated

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -34,6 +34,34 @@ export const list = async (ctx) => {
         ctx.throw(500, e);
     }
 };
-export const read = (ctx) => {};
-export const remove = (ctx) => {};
+
+/*  특정 포스트 조회
+    GET /api/posts/:id
+*/
+export const read = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*
+    DELETE /api/posts/:id
+*/
+export const remove = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 export const update = (ctx) => {};
