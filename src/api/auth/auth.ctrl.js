993b1de62c6ce49e851ed7c286e506d81280b7ce
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-24 17:51:17 +0900

    updated

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -93,4 +93,11 @@ export const check = async (ctx) => {
     }
     ctx.body = user;
 };
-export const logout = async (ctx) => {};
+
+/*
+    POST /api/auth/logout
+*/
+export const logout = async (ctx) => {
+    ctx.cookies.set('access_token');
+    ctx.status = 204; // No Content
+};
